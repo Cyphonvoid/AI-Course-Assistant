08e0faaef2a45c93745a7cd3c303a584fb4b7cc0
Cyphonvoid <yashaswi.kul@gmail.com>
2023-12-19 18:22:47 -0800

    Added basic selenium setup for scrapping

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AI-Course-Assistant
 An A.I academic companion to guide, assist and automate students in day to day academic tasks. 
+
+1) Update chrome browser and the chrome webdriver to same latest verion, and also always place chromewebdriver.exe in this directory
--- WebScrapper.py
+++ WebScrapper.py
@@ -1,2 +1,37 @@
-import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+#Path to web driver
+PATH = "chromedriver.exe"
+
+serv = Service(executable_path="chromedriver.exe")
+driver = webdriver.Chrome(service=serv)
+
+#URl to use from
+url = {
+    'google' : "https://www.google.com",
+    "nasa": "https://www.nasa.gov"
+}
+
+
+#Just getting the page of the url
+driver.get(url["google"])
+
+
+#printing the title
+print("Page Title: ", driver.title)
+
+
+#waiting
+time.sleep(4)
+
+
+#quiting
+driver.quit()
+
+
 
