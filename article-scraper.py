e248d3c026a9049406862f50a80edfcc0cf00bed
JaedonSpurlock01 <112233517+JaedonSpurlock01@users.noreply.github.com>
2023-12-21 21:23:20 -0800

    build: tested response with ai

--- article-scraper.py
+++ article-scraper.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from openai import OpenAI
+from gemini_test import *
 import time
 
 
@@ -24,10 +25,14 @@ element = driver.find_element(By.CLASS_NAME, "entry-content")
 # Grab the "insides" of the html element
 text = element.text
 
-print(text)
-
 #waiting
-time.sleep(4)
+time.sleep(1)
 
 #quiting
 driver.quit()
+
+user_question = "What is the following article about?\n"
+
+response = model.generate_content(user_question + text)
+
+print(user_question, response.text)
--- gemini_test.py
+++ gemini_test.py
@@ -3,13 +3,8 @@ import textwrap
 import os
 import google.generativeai as genai
 
-
-
-
 # Or use `os.getenv('GOOGLE_API_KEY')` to fetch an environment variable.
 GOOGLE_API_KEY= os.getenv('GOOGLE_API_KEY')
-
-
   
 genai.configure(api_key="AIzaSyD5LFvK9nIg-JJXI_lpVn4zsVNjUPCZz4w")
 for m in genai.list_models():
@@ -17,6 +12,3 @@ for m in genai.list_models():
     print(m.name)
 
 model = genai.GenerativeModel('gemini-pro')
-response = model.generate_content("What is the meaning of life?")
-
-print("MODEL: GEMINI-PRO ", response.text)
