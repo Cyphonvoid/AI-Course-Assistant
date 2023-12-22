ba8f0cf1bec93cd802689f75661433b1a5b2de3d
Cyphonvoid <yashaswi.kul@gmail.com>
2023-12-21 20:37:40 -0800

    New Gemini Api Update

---
+++ gemini_test.py
@@ -0,0 +1,22 @@
+import pathlib
+import textwrap
+import os
+import google.generativeai as genai
+
+
+
+
+# Or use `os.getenv('GOOGLE_API_KEY')` to fetch an environment variable.
+GOOGLE_API_KEY= os.getenv('GOOGLE_API_KEY')
+
+
+  
+genai.configure(api_key="AIzaSyD5LFvK9nIg-JJXI_lpVn4zsVNjUPCZz4w")
+for m in genai.list_models():
+  if 'generateContent' in m.supported_generation_methods:
+    print(m.name)
+
+model = genai.GenerativeModel('gemini-pro')
+response = model.generate_content("What is the meaning of life?")
+
+print("MODEL: GEMINI-PRO ", response.text)
